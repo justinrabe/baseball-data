9bb3fcc7f7fe2e59aebf3241e5ce5307560b1488
justin <jtr342@gmail.com>
2022-06-14 21:53:18 -0700

    extract_csv_zip parameterizes the CSV zip pull

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import requests
 from zipfile import ZipFile
 import io
 
-# Example dataset
+
+def extract_csv_zip (filename, myzip):
+    with myzip.open(filename) as myfile:
+        return pd.read_csv(myfile)
 
 ## TODO Parameterize CSV to grab each CSV. The Function should take the file name as input and output a pandas DF
 url = 'https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/v2022.2.zip'
 
 response = requests.get(url,  stream=True, verify=False)
 with ZipFile(io.BytesIO(response.content)) as myzip:
-    print(myzip)
-    print(myzip.namelist())
-    with myzip.open('baseballdatabank-2022.2/core/Pitching.csv') as myfile:
-        df = pd.read_csv(myfile)
+    df = extract_csv_zip('baseballdatabank-2022.2/core/Pitching.csv',myzip)
 
 print(df)
+
