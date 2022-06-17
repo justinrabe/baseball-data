5d7aa0367b25c5aa2d7a32698269880795a835f9
justin <jtr342@gmail.com>
2022-06-16 17:16:39 -0700

    planning rudimentary ETL

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def extract_csv_from_zip (filename, myzip):
         
 url = 'https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/v2022.2.zip'
 
+## potential to do: write a loop that grabs every CSV from the core folder
 response = requests.get(url,  stream=True, verify=False)
 with ZipFile(io.BytesIO(response.content)) as myzip:
     df_pitching = extract_csv_from_zip('baseballdatabank-2022.2/core/Pitching.csv',myzip)
@@ -20,4 +21,5 @@ with ZipFile(io.BytesIO(response.content)) as myzip:
     df_FieldingOF = extract_csv_from_zip('baseballdatabank-2022.2/core/FieldingOF.csv',myzip)
     df_FieldingOFsplit = extract_csv_from_zip('baseballdatabank-2022.2/core/FieldingOFsplit.csv',myzip)
 
-
+## perform insert into SQL DB
+## perform data transforms
