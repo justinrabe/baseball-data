9be1186bea63436dc43cce5f23d333b986df8796
justin <jtr342@gmail.com>
2022-06-17 18:33:00 -0700

    convert CSV to SQL schema using Pandas

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import requests
 from zipfile import ZipFile
+from sqlalchemy import create_engine
 import io
 
+##engine = create_engine('')
 
 def extract_csv_from_zip (filename, myzip):
     with myzip.open(filename) as myfile:
-        return pd.read_csv(myfile)
+        return pd.read_csv(myfile, nrows = 100)
         
 url = 'https://github.com/chadwickbureau/baseballdatabank/archive/refs/tags/v2022.2.zip'
 
-## potential to do: write a loop that grabs every CSV from the core folder
+## potential to do: write a loop that grabs every CSV from the core folder and converts to SQL schema
 response = requests.get(url,  stream=True, verify=False)
 with ZipFile(io.BytesIO(response.content)) as myzip:
     df_pitching = extract_csv_from_zip('baseballdatabank-2022.2/core/Pitching.csv',myzip)
@@ -21,5 +23,6 @@ with ZipFile(io.BytesIO(response.content)) as myzip:
     df_FieldingOF = extract_csv_from_zip('baseballdatabank-2022.2/core/FieldingOF.csv',myzip)
     df_FieldingOFsplit = extract_csv_from_zip('baseballdatabank-2022.2/core/FieldingOFsplit.csv',myzip)
 
+print(pd.io.sql.get_schema(df_pitching, name = 'pitching_data'))
 ## perform insert into SQL DB
 ## perform data transforms
