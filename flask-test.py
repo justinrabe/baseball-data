efb5fe65fd4162a8098a9fa2b004011a0f2f51a6
justin <jtr342@gmail.com>
2022-07-11 03:04:32 -0700

    successfully displayed data in Flask

--- flask-test.py
+++ flask-test.py
@@ -4,17 +4,14 @@ try:
     conn = psycopg2.connect(database="baseball_data", user="root", password = "root", host="localhost")
     print("Connection Successful!")
 except:
-    print("Connection Failed!")
+    raise Exception("Connection Failed!")
 curr = conn.cursor()
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
 
 def v_pitching():
     curr.execute("SELECT * FROM pitching limit 100")
     data = curr.fetchall()
     return render_template('pitching.html', data=data)
-
 
