076917520d1a12a7aea4c47ae9844e93a5d55653
justin <jtr342@gmail.com>
2022-07-27 22:15:15 -0700

    created sql script folder

---
+++ sql_queries/createPitching.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [IF NOT EXISTS] pitching (
+    playerID VARCHAR (20)
+     playerID  yearID  stint teamID  lgID   W   L   G  GS  CG  SHO  SV  IPouts    H   ER  HR  BB  SO  BAOpp    ERA  IBB  WP  HBP  BK   BFP  GF    R  SH  SF  GIDP
+)
